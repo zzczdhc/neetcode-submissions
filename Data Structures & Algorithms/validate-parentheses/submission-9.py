79322b5e94660447d8304b8a8413e0073532f7ed
Alex Zhang <166886454+zzczdhc@users.noreply.github.com>
2026-08-01 07:58:58 -0400

    Add: validate-parentheses - submission-9

---
+++ Data Structures & Algorithms/validate-parentheses/submission-9.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        mapping = {")":"(","]":"[","}":"{"}
+        stack = []
+        for i in s:
+            if i in mapping: # if it is a close parenthese
+                if stack and mapping[i] == stack[-1]:
+                    stack.pop()
+                else:
+                    return False
+            
+            else:
+                stack.append(i)#open
+        if stack:
+            return False
+        else:
+            return True
+        
+                
+            
+        
+                    
+
+
+
+
+
+              #  【】（） stack [(
+              #  （【】）
